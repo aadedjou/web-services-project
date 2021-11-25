ed8318197174083d560773f81d4459066c242dbf
adam <66301048+aadedjou@users.noreply.github.com>
2021-11-25 04:44:20 +0100

    Implemented console controller

---
+++ IfService/src/main/java/fr/uge/ifshare/rmi/common/controller/Choice.java
@@ -0,0 +1,88 @@
+package fr.uge.ifshare.rmi.common.controller;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Choice {
+    private int hintSize = 1;
+    private final String label;
+    private final Runnable action;
+
+    public Choice(String label, Runnable action) {
+        this.label = Objects.requireNonNull(label);
+        this.action = Objects.requireNonNull(action);
+    }
+
+    public Choice(String label) {
+        this(label, () -> System.out.println("'" + label + "' has been chosen."));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Choice)) return false;
+        Choice choice = (Choice) o;
+        return Objects.equals(label, choice.label);
+    }
+
+    public String getHint() {
+        return label.substring(0, hintSize);
+    }
+
+    public String getLabelWithoutHint() {
+        return label.substring(hintSize);
+    }
+
+    public void increaseHintSize() {
+        hintSize++;
+    }
+
+    public List<String> acceptedMatches() {
+        return List.of(
+          label, label.toLowerCase(), label.toUpperCase(Locale.ROOT),
+          getHint(), getHint().toLowerCase(), getHint().toUpperCase(Locale.ROOT)
+        );
+    }
+
+    public void processAction() {
+        action.run();
+    }
+}
+
+class ChoiceSet implements Iterable<Choice> {
+    private final LinkedHashSet<Choice> choices;
+
+    private ChoiceSet(Choice ... choices) {
+        this.choices = new LinkedHashSet<>(List.of(choices));
+        this.choices.forEach(Objects::requireNonNull);
+        ensureNoDuplicateHints();
+    }
+
+    public static ChoiceSet of(Choice ... choices) {
+        return new ChoiceSet(choices);
+    }
+
+    boolean hasDuplicateHints() {
+        var hintSet = choices.stream()
+                 .map(Choice::getHint)
+                 .collect(Collectors.toSet());
+        return choices.size() > hintSet.size();
+    }
+
+    void ensureNoDuplicateHints() {
+        while (hasDuplicateHints()) {
+            choices.forEach(c -> choices.forEach(c2 -> {
+                if (c.equals(c2)) return;
+                if (c.getHint().equals(c2.getHint())) {
+                    c.increaseHintSize();
+                    c2.increaseHintSize();
+                }
+            }));
+        }
+    }
+
+    @Override
+    public Iterator<Choice> iterator() {
+        return choices.iterator();
+    }
+}
---
+++ IfService/src/main/java/fr/uge/ifshare/rmi/common/controller/ConsoleController.java
@@ -0,0 +1,67 @@
+package fr.uge.ifshare.rmi.common.controller;
+
+import java.util.Scanner;
+import java.util.function.Predicate;
+
+public class ConsoleController {
+    private final static String BOLD_FONT = "\033[0;1m";
+    private final static String HINT_COLOR = "\033[38;2;175;150;250m";
+    private final static String ERROR_COLOR = "\033[38;2;250;50;100m";
+    private final static String DEFAULT_COLOR = "\u001B[0m";
+    private final static Scanner sc = new Scanner(System.in);
+
+    private static String errorMessage(String sequence) {
+        return ERROR_COLOR + sequence + DEFAULT_COLOR;
+    }
+
+    private static String bold(String sequence) {
+        return BOLD_FONT + sequence + DEFAULT_COLOR;
+    }
+
+    private static String hintedLabelOf(Choice choice) {
+        return HINT_COLOR + choice.getHint() + DEFAULT_COLOR + choice.getLabelWithoutHint();
+    }
+
+    public static void displayMenu(String message, Choice ... choices) {
+        Choice selectedChoice = null;
+        ChoiceSet choiceSet = ChoiceSet.of(choices);
+
+        System.out.println("\n\n" + bold(message) + "\n");
+        choiceSet.forEach(c -> System.out.println("   ➔ " + hintedLabelOf(c)));
+        System.out.println();
+
+        do {
+            System.out.print("> ");
+            String selection = sc.nextLine();
+
+            for (var choice : choices) {
+                if (choice.acceptedMatches().contains(selection)) {
+                    selectedChoice = choice;
+                    break;
+                }
+            }
+        } while (selectedChoice == null);
+
+        System.out.println();
+        selectedChoice.processAction();
+    }
+
+    public static String inputString(String message) {
+        return inputString(message, (s -> true), "Invalid information");
+    }
+
+    public static String inputString(String message, Predicate<String> condition, String errorMessage) {
+        System.out.println("\n   " + message);
+
+        System.out.print("> ");
+        var input = sc.nextLine();
+
+        while (!condition.test(input) || input.equals("")) {
+            if (!condition.test(input)) System.out.println(errorMessage(errorMessage));
+            System.out.print("> ");
+            input = sc.nextLine();
+        }
+        return input;
+    }
+}
+
