e19a820440054f3b6008ec570d850264b615b45e
adam <66301048+aadedjou@users.noreply.github.com>
2021-11-26 14:15:04 +0100

    Update console controller
    Has no color by default to avoid displaying problems
    Arrow is now classic

--- IfService/src/main/java/fr/uge/ifshare/rmi/common/controller/ConsoleController.java
+++ IfService/src/main/java/fr/uge/ifshare/rmi/common/controller/ConsoleController.java
@@ -4,30 +4,35 @@ import java.util.Scanner;
 import java.util.function.Predicate;
 
 public class ConsoleController {
-    private final static String BOLD_FONT = "\033[0;1m";
-    private final static String HINT_COLOR = "\033[38;2;175;150;250m";
-    private final static String ERROR_COLOR = "\033[38;2;250;50;100m";
-    private final static String DEFAULT_COLOR = "\u001B[0m";
     private final static Scanner sc = new Scanner(System.in);
 
-    private static String errorMessage(String sequence) {
-        return ERROR_COLOR + sequence + DEFAULT_COLOR;
+    public static ConsoleController standard() {
+        return new ConsoleController();
     }
 
-    private static String bold(String sequence) {
-        return BOLD_FONT + sequence + DEFAULT_COLOR;
+    public static ConsoleController colored() {
+        return new ColoredConsoleController();
     }
 
-    private static String hintedLabelOf(Choice choice) {
-        return HINT_COLOR + choice.getHint() + DEFAULT_COLOR + choice.getLabelWithoutHint();
+    String errorMessage(String sequence) {
+        return sequence;
     }
 
-    public static void displayMenu(String message, Choice ... choices) {
-        Choice selectedChoice = null;
+    String bold(String sequence) {
+        return sequence;
+    }
+
+    void printChoice(Choice choice) {
+        System.out.println(new String(new char[5 - choice.getHint().length()]).replace("\0", " ") +
+                              "(" + choice.getHint().toLowerCase() + ") > " + choice.getLabel());
+    }
+
+    public void displayMenu(String message, Choice... choices) {
         ChoiceSet choiceSet = ChoiceSet.of(choices);
+        Choice selectedChoice = null;
 
         System.out.println("\n\n" + bold(message) + "\n");
-        choiceSet.forEach(c -> System.out.println("   ➔ " + hintedLabelOf(c)));
+        choiceSet.forEach(this::printChoice);
         System.out.println();
 
         do {
@@ -46,11 +51,11 @@ public class ConsoleController {
         selectedChoice.processAction();
     }
 
-    public static String inputString(String message) {
+    public String inputString(String message) {
         return inputString(message, (s -> true), "Invalid information");
     }
 
-    public static String inputString(String message, Predicate<String> condition, String errorMessage) {
+    public String inputString(String message, Predicate<String> condition, String errorMessage) {
         System.out.println("\n   " + message);
 
         System.out.print("> ");
